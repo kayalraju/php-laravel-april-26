6dfb78becdf06d99ca35ec7423327d2418e5b34e
kayalraju <kayalraju003@gmail.com>
2026-05-12 12:10:38 +0530

    operator and function

---
+++ mysql/operatorandfunc.php
@@ -0,0 +1,38 @@
+<?php
+include "connection.php";
+
+
+//select all data
+// $stmt = $pdo->prepare("SELECT * FROM employees");
+// $stmt->execute();
+// $employees = $stmt->fetchAll();
+
+//or operator
+// $stmt = $pdo->prepare("SELECT * FROM employees WHERE department = 'IT' OR department = 'HR'");
+// $stmt->execute();
+// $employees = $stmt->fetchAll();
+
+
+//and operator
+// $stmt = $pdo->prepare("SELECT * FROM employees WHERE department = 'Reactjs' AND salary > 35000");
+// $stmt->execute();
+// $employees = $stmt->fetchAll();
+
+//not operator
+// $stmt = $pdo->prepare("SELECT * FROM employees WHERE NOT department = 'HR'");
+// $stmt->execute();
+// $employees = $stmt->fetchAll();
+
+//min function
+$stmt = $pdo->prepare("SELECT MIN(salary) AS minimum_salary FROM employees");
+$stmt->execute();
+$employees = $stmt->fetch();
+
+
+echo "<pre>";
+print_r($employees);
+echo "</pre>";
+
+
+
+?>
