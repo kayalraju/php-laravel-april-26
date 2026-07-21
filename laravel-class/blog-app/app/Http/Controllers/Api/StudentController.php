35e0e5886fe6cc12dca067fe6941d877094dd257
LearningDeVops-png <kayalraju004@gmail.com>
2026-07-21 15:41:55 +0530

    api

--- laravel-class/blog-app/app/Http/Controllers/Api/StudentController.php
+++ laravel-class/blog-app/app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -23,24 +24,60 @@ class StudentController extends Controller
 
     public function createStudent(Request $request)
     {
-        $request->validate([
-            'name' => 'required|max:50',
-            'email' => 'required',
-            'phone' => 'required',
+        // $request->validate([
+        //     'name' => 'required|max:50',
+        //     'email' => 'required',
+        //     'phone' => 'required',
            
+        // ]);
+        // // Proceed with saving the product if validation passes
+        // $student = new Student();
+        // $student->name = $request->name;
+        // $student->email = $request->email;
+        // $student->phone = $request->phone;;
+        // $student->save();
+
+        // return response()->json([
+        //     'status' => true,
+        //     'message' => 'student created successfully',
+        //     'data' => $student
+        // ], 201);
+        try {
+
+        $validator = Validator::make($request->all(), [
+            'name'  => 'required|max:50',
+            'email' => 'required|email|unique:students,email',
+            'phone' => 'required|digits:10',
         ]);
-        // Proceed with saving the product if validation passes
-        $student = new Student();
-        $student->name = $request->name;
-        $student->email = $request->email;
-        $student->phone = $request->phone;;
-        $student->save();
-        
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $student = Student::create([
+            'name'  => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+        ]);
+
         return response()->json([
             'status' => true,
-            'message' => 'student created successfully',
+            'message' => 'Student created successfully',
             'data' => $student
         ], 201);
+
+    } catch (\Exception $e) {
+
+        return response()->json([
+            'status' => false,
+            'message' => 'Something went wrong',
+            'error' => $e->getMessage()
+        ], 500);
+    }
     }
 }
 
